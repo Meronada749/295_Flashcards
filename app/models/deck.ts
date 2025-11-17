00ef0b0445d00e7436e8a5e9e05691ed3f12a780
Meronada749 <meron.essayas@eduvaud.ch>
2025-11-17 11:28:49 +0100

    feat: Add deck and card models with migrations
    Introduced Deck and Card models for AdonisJS, including their respective database migration files. Also renamed the users table migration for consistency, removed a border from the .container class in app.css, and added a commented route example in routes.ts.

---
+++ app/models/deck.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+// import type { HasMany } from '@adonisjs/lucid/types/relations'
+// import Card from './card.js'
+
+export default class Deck extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare published: boolean
+
+  // @hasMany(() => Card)
+  // declare books: HasMany<typeof Card>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/3_create_cards_table.ts
@@ -0,0 +1,29 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'cards'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('question')
+      table.string('response')
+
+      table
+        .integer('deck_id') // Clé étrangère
+        .unsigned() // La clé ne doit pas être négative
+        .references('id') // Référence la colonne `id` de la table `deck`
+        .inTable('deck') // Nom de la table de référence
+        .onDelete('CASCADE') // Supprime les teachers si la deck est supprimée
+        .onUpdate('CASCADE') // Met à jour la clé étrangère si l'id change
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
