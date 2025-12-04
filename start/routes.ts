ca5edb045544ee5d9b077b6629b93d5f56e4273d
Meronada749 <meron.essayas@eduvaud.ch>
2025-12-04 22:55:47 +0100

    feat: Add flashcard flip UI and progress tracking
    Enhanced the card view with a flip animation and keyboard/click support for flipping cards. Updated deck play view to show progress and added answer buttons. Secured deck and card routes with authentication middleware. Added x.html as a UI prototype for flashcard navigation and interaction.

--- start/routes.ts
+++ start/routes.ts
@@ -24,47 +24,72 @@ router.get('/', [DecksController, 'index']).as('home')
 router.get('/decks-by-user/show', [DecksController, 'show']).as('decks.show').use(middleware.auth())
 
 // Route permettant de voir les cards d'un deck
-router.get('/deck/:deck_id/show', [CardsController, 'show']).as('cards.show')
+router.get('/deck/:deck_id/show', [CardsController, 'show']).as('cards.show').use(middleware.auth())
 
 // Route permettant de supprimer les decks d'un user
-router.delete('/deck/:deck_id/destroy', [DecksController, 'destroy']).as('deck.destroy')
+router
+  .delete('/deck/:deck_id/destroy', [DecksController, 'destroy'])
+  .as('deck.destroy')
+  .use(middleware.auth())
 
 // Route permettant d'afficher le formulaire permettant l'ajout d'un deck
-router.get('/deck/add', [DecksController, 'create']).as('deck.create')
+router.get('/deck/add', [DecksController, 'create']).as('deck.create').use(middleware.auth())
 // Route permettant l'ajout d'un deck
-router.post('/deck/add', [DecksController, 'store']).as('deck.store')
+router.post('/deck/add', [DecksController, 'store']).as('deck.store').use(middleware.auth())
 
 // Route permettant d'afficher le formulaire permettant la mise à jour d'un deck
-router.get('/deck/:deck_id/edit', [DecksController, 'edit']).as('deck.edit')
+router.get('/deck/:deck_id/edit', [DecksController, 'edit']).as('deck.edit').use(middleware.auth())
 // Route permettant la modification d'un deck
-router.post('/deck/:deck_id/update', [DecksController, 'update']).as('deck.update')
+router
+  .post('/deck/:deck_id/update', [DecksController, 'update'])
+  .as('deck.update')
+  .use(middleware.auth())
 
 //router.get('/deck/:deck_id/play', [DecksController, 'play']).as('deck.play')
-router.get('/deck/:deck_id/play/:index', [CardsController, 'play']).as('deck.play')
+router.get('/deck/:deck_id/play', [CardsController, 'play']).as('deck.play').use(middleware.auth())
 
 /**
  * ROUTES pour les CARDS
  */
 
 // Route permettant de voir une carte d'un deck
-router.get('/deck/:deck_id/cards/:card_id/show', [CardsController, 'showCard']).as('card.show')
+router
+  .get('/deck/:deck_id/cards/:card_id/show', [CardsController, 'showCard'])
+  .as('card.show')
+  .use(middleware.auth())
 
 // Route permettant de supprimer une carte d'un deck
 router
   .delete('/decks/:deck_id/cards/:card_id/destroy', [CardsController, 'destroy'])
   .as('card.destroy')
+  .use(middleware.auth())
 
 // Route permettant d'afficher le formulaire permettant l'ajout d'une carte
-router.get('/decks/:deck_id/card/add', [CardsController, 'create']).as('card.create')
+router
+  .get('/decks/:deck_id/card/add', [CardsController, 'create'])
+  .as('card.create')
+  .use(middleware.auth())
 // Route permettant l'ajout d'une carte
-router.post('/decks/:deck_id/card/add', [CardsController, 'store']).as('card.store')
+router
+  .post('/decks/:deck_id/card/add', [CardsController, 'store'])
+  .as('card.store')
+  .use(middleware.auth())
 
 // Route permettant de modifier une carte d'un deck
-router.get('/decks/:deck_id/card/:card_id/edit', [CardsController, 'edit']).as('card.edit')
+router
+  .get('/decks/:deck_id/card/:card_id/edit', [CardsController, 'edit'])
+  .as('card.edit')
+  .use(middleware.auth())
 // Route permettant la modification d'une carte
-router.post('/decks/:deck_id/card/:card_id/update', [CardsController, 'update']).as('card.update')
+router
+  .post('/decks/:deck_id/card/:card_id/update', [CardsController, 'update'])
+  .as('card.update')
+  .use(middleware.auth())
 
 // Route permettant de se connecter
 router.post('/login', [AuthController, 'handleLogin']).as('auth.handleLogin')
 // Route permettant de se déconnecter
-router.post('/logout', [AuthController, 'handleLogout']).as('auth.handleLogout')
+router
+  .post('/logout', [AuthController, 'handleLogout'])
+  .as('auth.handleLogout')
+  .use(middleware.auth())
